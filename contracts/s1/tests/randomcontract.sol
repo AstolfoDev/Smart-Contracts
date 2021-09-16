9ea31735eb8bb300a992d12dc9a919ab53700e3c
owouwuowo <digitalgpro@gmail.com>
2021-09-16 19:50:09 +0000

    Specified unsigned integers to uint256 and encoded block difficulties.

--- contracts/s1/tests/randomcontract.sol
+++ contracts/s1/tests/randomcontract.sol
@@ -8,41 +8,53 @@ pragma solidity ^0.4.17;
 contract randomcontract {
     
     // Creates starting 'random' variable with current block diff.
-    uint private randomvalue = generateValue();
-    mapping(uint => bool) private history;
+    uint256 private randomvalue = generateValue();
+    mapping(uint256 => bool) private history;
     
     // Function pushes value to the end of the history storage.
-    function pushToHistory(uint value) private {
+    function pushToHistory(uint256 value) private {
         history[value] = true;
     }
     
     // Function checks if value exists in the history storage.
-    function getHistory(uint value) public view returns (bool) {
+    function getHistory(uint256 value) public view returns (bool) {
         return history[value];
     }
     
     // Function regens the 'random' variable with the latest block diff. until a value that hasn't been used in history is found.
-    function generateValue() public returns (uint) {
-        randomvalue = uint(keccak256(block.difficulty, now));
+    function generateValue() public returns (uint256) {
+        
+        randomvalue = uint256 (
+            keccak256 (
+                abi.encodePacked(block.difficulty, now)
+            )
+        );
+        
         while (history[randomvalue] == true) {
-            randomvalue = uint(keccak256(block.difficulty, now));
+            randomvalue = uint256 (
+                keccak256 (
+                    abi.encodePacked(block.difficulty, now)
+                )
+            );
         }
+        
         pushToHistory(randomvalue);
+        
         return randomvalue;
     }
     
     // Function returns the last saved block diff.
-    function getValue() public view returns (uint) {
+    function getValue() public view returns (uint256) {
         return randomvalue;
     }
     
     // Function uses the last saved block diff. to return number < max.
-    function getWithin(uint max) public view returns (uint) {
+    function getWithin(uint256 max) public view returns (uint256) {
         return randomvalue % max;
     }
     
     // Function regens the 'random' variable and uses latest block diff. to return number < max.
-    function getWithinGen(uint max) public returns (uint) {
+    function getWithinGen(uint256 max) public returns (uint256) {
         generateValue();
         return randomvalue % max;
     }
